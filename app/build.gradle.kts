688d4087c0b6135154effe701c9833e5d5a999da
kennark <kennar.kahju@gmail.com>
2026-09-21 13:21:11 +0300

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gasodoapp.gasodo"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

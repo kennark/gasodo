02358d25043cc73175cc97b25f2e8e2bac21ba7b
kennark <kennar.kahju@gmail.com>
2026-09-22 15:24:27 +0300

    Scrollable overview screen

--- app/src/main/java/com/gasodoapp/gasodo/feature/overview/OverviewScreen.kt
+++ app/src/main/java/com/gasodoapp/gasodo/feature/overview/OverviewScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DatePickerDialog
@@ -153,7 +155,8 @@ private fun MainContent(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
